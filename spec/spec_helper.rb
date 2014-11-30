1044ce5defd6ae47dfa18ded208b865ccad14bd1
pythogorian <nino.mil@gmail.com>
2014-11-30 17:09:03 +0100

    Update code climate configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,8 @@ require 'rspec'
 require 'codeclimate-test-reporter'
 require 'trange_frange'
 
+CodeClimate::TestReporter.start
+
 include TrangeFrange
 
-ENV['CODECLIMATE_REPO_TOKEN'] = '5dd60a5cd7798f1af725393701b267e5b4366c7ef895c6643746735c5645ef91'
-CodeClimate::TestReporter.start
+
